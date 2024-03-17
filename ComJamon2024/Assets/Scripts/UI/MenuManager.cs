d540f4eebf122ceb3915a86c34f62bac26379b19
Eric11196 <izandeve@ucm.es>
2024-03-17 22:18:10 +0100

    Updated

--- ComJamon2024/Assets/Scripts/UI/MenuManager.cs
+++ ComJamon2024/Assets/Scripts/UI/MenuManager.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
-using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class MenuManager : MonoBehaviour
 {
